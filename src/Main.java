22d2ff6da1580477fa39c1cc9b1a1f2e528320b6
TIME <opout777@yandex.ru>
2023-01-13 15:19:38 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,26 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
+
     public static void task1 () {
         System.out.println("Задача 1");
+
+        int one = 70000;
+        System.out.println(one);
+
+        byte two = 115;
+        System.out.println(two);
+
+        short three = 21000;
+        System.out.println(three);
+
+        long four = 3000000000L;
+        System.out.println(four);
+
+        float five = 17.4E+15F;
+        System.out.println(five);
+
+        double six = 9.4E+200;
+        System.out.println(six);
     }
 }
